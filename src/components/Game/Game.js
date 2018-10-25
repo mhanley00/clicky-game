29424655dac376a282fdc3789b3d068afc361358
mhanley00 <mehanley2@gmail.com>
2018-10-25 17:12:20 -0400

    still not working, will crash on click

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -20,66 +20,66 @@ class Game extends Component {
 //   componentDidMount(){
 //       this.setState({pokemon:this.shuffleData(this.state.pokemon)}); //CHANGE
 //   }
-//   handleCorrectGuess = newData => {
-//       const {topScore, score} = this.state;
-//       const newScore = score + 1;
-//       const newTopScore = Math.max(newScore, topScore); //keeping the best score in the Nav
+  handleCorrectGuess = newData => {
+      const {topScore, score} = this.state;
+      const newScore = score + 1;
+      const newTopScore = Math.max(newScore, topScore); //keeping the best score in the Nav
 
-//       this.setState({//CHANGE
-//         pokemon: this.shuffleData(newData), //newData is the props that we're bringing in from arrow func
-//           score: newScore,
-//           topScore: newTopScore
-//       });
-//   };
+      this.setState({//CHANGE
+        pokemon: this.shuffleData(newData), //newData is the props that we're bringing in from arrow func
+          score: newScore,
+          topScore: newTopScore
+      });
+  };
 
-//   handleIncorrectGuess = otherData => {
-//       this.setState({
-//         pokemon: this.shuffleData(otherData), //CHANGE
-//           score: 0
+  handleIncorrectGuess = otherData => {
+      this.setState({
+        pokemon: this.shuffleData(otherData), //CHANGE
+          score: 0
 
-//       });
-//   };
+      });
+  };
 
-//   resetGame = data => { //does not change
-//       const resetData = data.map(item =>
-//         ({...item, clicked:false}))
-//         return this.shuffleData(resetData);
-//   };
+  resetGame = data => { //does not change
+      const resetData = data.map(item =>
+        ({...item, clicked:false}))
+        return this.shuffleData(resetData);
+  };
 
-//   shuffleData = data => {//does not change
-//       let i = data.length - 1;
-//       while (i>0){
-//           const j = Math.floor(Math.random()*(i +1));
-//           const temp = data[i];
-//           data[i] = data[j];
-//           data[j] = temp;
-//       }
-//       return data;
-//   };
+  shuffleData = data => {//does not change
+      let i = data.length - 1;
+      while (i>0){
+          const j = Math.floor(Math.random()*(i +1));
+          const temp = data[i];
+          data[i] = data[j];
+          data[j] = temp;
+      }
+      return data;
+  };
 
-//   handleItemClick = id => {
-//       let guessedCorrectly = false;
-//       const newData = this.state.pokemon.map(item =>
-//         {
-//             const newItem = {...item};
-//             if  (newItem.id === id){
-//                 if (!newItem.clicked){
-//                     newItem.clicked = true;
-//                     guessedCorrectly = true;
-//                     //updating the state here once the user has clicked
-//                 }
+  handleItemClick = id => {
+      let guessedCorrectly = false;
+      const newData = this.state.pokemon.map(item =>
+        {
+            const newItem = {...item};
+            if  (newItem.id === id){
+                if (!newItem.clicked){
+                    newItem.clicked = true;
+                    guessedCorrectly = true;
+                    //updating the state here once the user has clicked
+                }
 
                 
-//             }
-//             return newItem;
+            }
+            return newItem;
 
-//         });
-//         guessedCorrectly //this is the condition of the if statement, if true
-//             ? this.handleCorrectGuess(newData)
-//             //fancy if/else statement, ie if guessedCorrectly is true, then handleCorrectGuess data
-//             : this.handleIncorrectGuess(newData);
-//             //else is ":""
-//   };
+        });
+        guessedCorrectly //this is the condition of the if statement, if true
+            ? this.handleCorrectGuess(newData)
+            //fancy if/else statement, ie if guessedCorrectly is true, then handleCorrectGuess data
+            : this.handleIncorrectGuess(newData);
+            //else is ":""
+  };
   
   
   render() { //part of Component
