006436e22abe941f2e49b282f02c966db7077fc5
mhanley00 <mehanley2@gmail.com>
2018-10-25 16:58:21 -0400

    styling, pokemon

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Container.css"; 
+import pokeforest from './pokeforest.jpg'
 
-const Container = props => <main className="container">{props.children}</main>
+
+const Container = props => 
+<div>
+ <main className="container">{props.children}
+{/* <img src={pokeforest} alt={"logo"}/> 
+had major trouble getting this to display*/}
+  </main>
+ </div>
+
 
 export default Container;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,9 @@ import "./Header.css";
 
 const Header = props => (
   <header className="header">
-    <h1>Pokemon Memory Game!</h1>
+    <h1>PokéMemory: The Pokémon Memory Game!</h1>
     <h2>
-      Click on each Pokemon once to earn points.
+      Click on each Pokémon once to earn points.
     </h2>
   </header>
 );
